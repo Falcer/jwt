cdda6e580b5a991e7e840e45b659282beb379cfa
Argana Phangquestian <arganaphang@gmail.com>
2021-01-16 16:21:53 +0700

    Git Rest API

---
+++ NOTE.md
@@ -0,0 +1,9 @@
+### Go framework:
+1. Gorilla/mux 
+2. gin gonic
+3. fiber
+
+### Learning???:
+1. Testing (Unit testing, Integrasion testing, end to end testing)
+2. Library
+3. Distributed + Concurrent + Loading Balance (Caddy Server + SSL + HTTP2)
--- server/main.go
+++ server/main.go
@@ -6,15 +6,77 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// User struct
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// Fake table
+var users []User
+
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(&fiber.Map{
+	app.Get("/users", usersHandler)
+	app.Post("/login", loginHandler)
+	app.Post("/register", registerHandler)
+
+	log.Fatal(app.Listen(":8080"))
+}
+
+func usersHandler(c *fiber.Ctx) error {
+	return c.Status(200).JSON(&fiber.Map{
+		"success": true,
+		"message": "Get All Users",
+		"data":    users,
+	})
+}
+
+func loginHandler(c *fiber.Ctx) error {
+	// Login Logic
+	u := new(User)
+
+	if err := c.BodyParser(u); err != nil {
+		return c.Status(500).JSON(&fiber.Map{
 			"success": false,
-			"message": "mantap!",
+			"message": "Login Failed",
 		})
+	}
+
+	for _, user := range users {
+		if (*u).Username == user.Username && (*u).Password == user.Password {
+			return c.Status(200).JSON(&fiber.Map{
+				"success": true,
+				"message": "Login successfully",
+				"data":    u,
+			})
+		}
+	}
+
+	return c.Status(404).JSON(&fiber.Map{
+		"success": false,
+		"message": "Username or Password not correct",
 	})
 
-	log.Fatal(app.Listen(":3000"))
+}
+
+func registerHandler(c *fiber.Ctx) error {
+	// Register Logic
+	u := new(User)
+
+	if err := c.BodyParser(u); err != nil {
+		return c.Status(500).JSON(&fiber.Map{
+			"success": false,
+			"message": "Register Failed",
+		})
+	}
+
+	users = append(users, *u)
+
+	return c.Status(200).JSON(&fiber.Map{
+		"success": true,
+		"message": "Register",
+		"data":    u,
+	})
 }
